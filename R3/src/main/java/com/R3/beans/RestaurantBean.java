95690c4dab22e93bd9f30eb01643f95d43cfe204
Nick ruha <you@example.com>
2017-08-24 13:51:38 -0400

    userbeans

--- R3/src/main/java/com/R3/beans/RestaurantBean.java
+++ R3/src/main/java/com/R3/beans/RestaurantBean.java
@@ -1,5 +1,7 @@
 package com.R3.beans;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -30,8 +32,132 @@ public class RestaurantBean {
 	@Column(name="RESTAURANT_PHONE_NUMBER")
 	private int phoneNumber;
 	@Column(name="RESTAURANT_DATE_ESTABLISHED")
-	private? foundingDate
+	private ? foundingDate
 	@ManyToOne
 	@Column(name="RESTAURANT_DESCRIPTION")
 	private String description;
+	
+	private List<ReviewBean> allReviews;
+
+	
+	public RestaurantBean(){
+		super();
+	}
+	
+	public RestaurantBean(int restaurantId, int ownerId, int typeId, int addressId, int phoneNumber,
+			String description) {
+		super();
+		this.restaurantId = restaurantId;
+		this.ownerId = ownerId;
+		this.typeId = typeId;
+		this.addressId = addressId;
+		this.phoneNumber = phoneNumber;
+		this.description = description;
+	}
+
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	public int getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public int getTypeId() {
+		return typeId;
+	}
+
+	public void setTypeId(int typeId) {
+		this.typeId = typeId;
+	}
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<ReviewBean> getAllReviews() {
+		return allReviews;
+	}
+
+	public void setAllReviews(List<ReviewBean> allReviews) {
+		this.allReviews = allReviews;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + addressId;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ownerId;
+		result = prime * result + phoneNumber;
+		result = prime * result + restaurantId;
+		result = prime * result + typeId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RestaurantBean other = (RestaurantBean) obj;
+		if (addressId != other.addressId)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (ownerId != other.ownerId)
+			return false;
+		if (phoneNumber != other.phoneNumber)
+			return false;
+		if (restaurantId != other.restaurantId)
+			return false;
+		if (typeId != other.typeId)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "RestaurantBean [restaurantId=" + restaurantId + ", ownerId=" + ownerId + ", typeId=" + typeId
+				+ ", addressId=" + addressId + ", phoneNumber=" + phoneNumber + ", description=" + description + "]";
+	}
+	
+	
+	
+	
 }
--- R3/src/main/java/com/R3/beans/ReviewBean.java
+++ R3/src/main/java/com/R3/beans/ReviewBean.java
@@ -18,6 +18,7 @@ public class ReviewBean {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="REVIEW_ID")
+	@ManyToOne
 	private int id;
 	@Column(name="REVIEW_RATING")
 	private int rating;
@@ -29,5 +30,97 @@ public class ReviewBean {
 	private int restaurantId;
 	@Column(name="USER_ID")
 	private int userId;
+	
+	
+	
+	public ReviewBean(){
+		super();
+	}
+	
+	public ReviewBean(int id, int rating, String description, int restaurantId, int userId) {
+		super();
+		this.id = id;
+		this.rating = rating;
+		this.description = description;
+		this.restaurantId = restaurantId;
+		this.userId = userId;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + rating;
+		result = prime * result + restaurantId;
+		result = prime * result + userId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReviewBean other = (ReviewBean) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (rating != other.rating)
+			return false;
+		if (restaurantId != other.restaurantId)
+			return false;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ReviewBean [id=" + id + ", rating=" + rating + ", description=" + description + ", restaurantId="
+				+ restaurantId + ", userId=" + userId + "]";
+	}
+
+	
+	
+	
+	
 }
