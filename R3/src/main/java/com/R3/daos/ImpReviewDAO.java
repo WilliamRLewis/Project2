256502a615cc54925bff5177ae80969a14bf184d
William <WilliamNotMyEmail@gmail.com>
2017-08-24 13:51:47 -0400

    started work on ReviewDAO + tests

---
+++ R3/src/main/java/com/R3/daos/ImpReviewDAO.java
@@ -0,0 +1,60 @@
+package com.R3.daos;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.R3.beans.RestaurantBean;
+import com.R3.beans.ReviewBean;
+import com.R3.beans.UserBean;
+
+public class ImpReviewDAO implements ReviewDAO {
+	private SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	@Override
+	public void create(ReviewBean review) {
+		sessionFactory.getCurrentSession().save(review);
+
+	}
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	@Override
+	public void update(ReviewBean review) {
+		sessionFactory.getCurrentSession().update(review);
+
+	}
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	@Override
+	public void delete(ReviewBean review) {
+		sessionFactory.getCurrentSession().delete(review);
+
+	}
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	@Override
+	public ReviewBean find(ReviewBean review) {
+		throw new UnsupportedOperationException(); 
+	}
+
+	@Override
+	public List<ReviewBean> findOnRestaurant(RestaurantBean restaurant) {
+		throw new UnsupportedOperationException(); 
+	}
+
+	@Override
+	public List<ReviewBean> findOnUser(UserBean user) {
+		throw new UnsupportedOperationException(); 
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ReviewBean> findAll() {
+		return sessionFactory.getCurrentSession().createCriteria(ReviewBean.class).list();
+	}
+
+}
