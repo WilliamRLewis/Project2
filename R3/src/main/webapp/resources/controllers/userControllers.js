fecb68d5d790e2aa34cdd628f95d552f51dce528
William <WilliamNotMyEmail@gmail.com>
2017-08-29 18:22:10 -0400

    Front end fiddlin with users

--- R3/src/main/webapp/resources/controllers/userControllers.js
+++ R3/src/main/webapp/resources/controllers/userControllers.js
@@ -1,14 +1,17 @@
 angular.module("R3App")
-.controller("findUsers", function($http, $scope){
+.controller("findUsers", function($http, $scope, $location){
+		$scope.data = {};
 		$scope.getUsers = function(){
 				alert("About to attempt get!")
 			$http.get("user/all").success(function (data) {
-		         alert("Winner")
+				$scope.data = data;
 		     })
 		     .error(function (error) {
 		         alert("Loser");
 		     });
 	}
-	$scope.test = function(){
-		alert("Success!");
+		$scope.loadReviews = function(){
+			$location.path("/userReviews");
 	}
+});
+
